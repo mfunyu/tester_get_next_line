cfdf672caa612461f530154a686bb5e7b9907aab
mfunyu <u_2@mfunyus-MacBook-Pro.local>
2020-07-08 13:58:51 +0900

    main4bonus

--- gnl_main_bonus.c
+++ gnl_main_bonus.c
@@ -12,7 +12,6 @@ int		main(int ac, char **av)
 	int		fd2 = 0;
 	int		fd3 = 0;
 	int		fd4 = 0;
-	// int 	i = 0;
 	int 	ret;
 
 	if (ac > 1)
@@ -22,42 +21,29 @@ int		main(int ac, char **av)
 		fd3 = open(av[3], O_RDONLY);
 		fd4 = open(av[4], O_RDONLY);
 	}
-	// if (fd < 0)
-	// 	printf("file name error\n");
-	// while (1)
-	// while (i++ < 23)
-	// {
-		ret = get_next_line(fd, &line);
-		printf("fd=%d : %s", fd, line);
-		printf("(%d)\n", ret);
-		ret = get_next_line(fd, &line);
-		printf("fd=%d : %s", fd, line);
-		printf("(%d)\n", ret);
-		ret = get_next_line(fd4, &line);
-		printf("fd=%d : %s", fd4, line);
-		printf("(%d)\n", ret);
-		ret = get_next_line(fd2, &line);
-		printf("fd=%d : %s", fd2, line);
-		printf("(%d)\n", ret);
-		ret = get_next_line(fd3, &line);
-		printf("fd=%d : %s", fd3, line);
-		printf("(%d)\n", ret);
-		ret = get_next_line(fd, &line);
-		printf("fd=%d : %s", fd, line);
-		printf("(%d)\n", ret);
-		// if (ret == -1)
-		// {
-		// 	printf("ret: %d (error ditected)\n", ret);
-		// 	break ;
-		// }
-		// else
-		// {
-		// 	if (ret == 0)
-		// 	{
-		// 		break ;
-		// 	}
-		// 	printf("%s\n", line);
-		// }
-	// }
-	// while(1){}
+	//fd---------------------------------//
+	ret = get_next_line(fd, &line);
+	printf("fd=%d : %s", fd, line);
+	printf("(%d)\n", ret);
+	//fd---------------------------------//
+	ret = get_next_line(fd, &line);
+	printf("fd=%d : %s", fd, line);
+	printf("(%d)\n", ret);
+	//fd which dosen't not exitst------//
+	ret = get_next_line(fd4, &line);
+	printf("fd=%d : %s", fd4, line);
+	printf("(%d)\n", ret);
+	//fd2---------------------------------//
+	ret = get_next_line(fd2, &line);
+	printf("fd=%d : %s", fd2, line);
+	printf("(%d)\n", ret);
+	//fd3---------------------------------//
+	ret = get_next_line(fd3, &line);
+	printf("fd=%d : %s", fd3, line);
+	printf("(%d)\n", ret);
+	//fd---------------------------------//
+	ret = get_next_line(fd, &line);
+	printf("fd=%d : %s", fd, line);
+	printf("(%d)\n", ret);
+
 }
