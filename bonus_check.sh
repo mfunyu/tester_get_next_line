04287a1555fab9fd90d63ef0fa2e6907c0dcf45f
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-29 22:38:51 +0900

    [clean] bonus tests

--- bonus_check.sh
+++ bonus_check.sh
@@ -1,39 +1,22 @@
 
 make -s fclean
 
+# define
+CYAN="\033[1;36m"
+RESET="\033[m"
+
 printf "<<------------bonus----------------->>\n"
 
-#0-19-------------------------------------------------------------------------------------
+function test()
+{
+	printf "$CYAN[BUFFER_SIZE='$i']$RESET : \n"
+	make -sB ARG=$i bonus 2> /dev/null
+	./bonus txts/a txts/b txts/c
+}
 
-for ((i=0 ; i < 10; i++))
-do
-printf '\e[36;1m[BUFFER_SIZE='$i']\e[m'
-make clean -s ARG=$i bonus
-printf " : \n"
-./bonus txts/a txts/b txts/c
+for ((i=0 ; i < 10; i++)); do
+	test $i
 done
-
-#any numbers-------------------------------------------------------------------------------------
-
-for i in 32 1024 1025 9999 100000 10000000
-do
-printf '\e[36;1m[BUFFER_SIZE='$i']\e[m'
-make clean -s ARG=$i bonus
-printf " : \n"
-./bonus txts/a txts/b txts/c
+for i in 32 1024 1025 9999 100000 10000000; do
+	test $i
 done
-
-
-#STDinput-------------------------------------------------------------------------------------
-
-printf "<<------------STD input----------------->>\n"
-
-for i in 32 1024 1025 9999 100000 10000000
-do
-printf '\e[36;1m[BUFFER_SIZE='$i']\e[m'
-make clean -s ARG=$i bonus
-printf " : \n"
-./bonus
-done
-
-# make -s clean
