85cc59e193d81ff7e91d0e83315c0fe6d4016618
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-29 22:32:25 +0900

    [update] alterate for new subject requirements

--- gnl_main_bonus.c
+++ gnl_main_bonus.c
@@ -2,48 +2,51 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "get_next_line.h"
 
-int		get_next_line(int fd, char **line);
-
-int		main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	char	*line;
-	int		fd = 0;
+	int		fd1 = 0;
 	int		fd2 = 0;
 	int		fd3 = 0;
 	int		fd4 = 0;
-	int 	ret;
 
 	if (ac > 1)
 	{
-		fd = open(av[1], O_RDONLY);
+		fd1 = open(av[1], O_RDONLY);
 		fd2 = open(av[2], O_RDONLY);
 		fd3 = open(av[3], O_RDONLY);
 		fd4 = open(av[4], O_RDONLY);
 	}
-	//fd---------------------------------//
-	ret = get_next_line(fd, &line);
-	printf("fd=%d : %s", fd, line);
-	printf("(%d)\n", ret);
-	//fd---------------------------------//
-	ret = get_next_line(fd, &line);
-	printf("fd=%d : %s", fd, line);
-	printf("(%d)\n", ret);
+	//fd1---------------------------------//
+	line = get_next_line(fd1);
+	printf("fd=%d : %s", fd1, line);
+	if (!line)
+		printf("\n");
+	//fd1---------------------------------//
+	line = get_next_line(fd1);
+	printf("fd=%d : %s", fd1, line);
+	if (!line)
+		printf("\n");
 	//fd which dosen't not exitst------//
-	ret = get_next_line(fd4, &line);
+	line = get_next_line(fd4);
 	printf("fd=%d : %s", fd4, line);
-	printf("(%d)\n", ret);
+	if (!line)
+		printf("\n");
 	//fd2---------------------------------//
-	ret = get_next_line(fd2, &line);
+	line = get_next_line(fd2);
 	printf("fd=%d : %s", fd2, line);
-	printf("(%d)\n", ret);
+	if (!line)
+		printf("\n");
 	//fd3---------------------------------//
-	ret = get_next_line(fd3, &line);
+	line = get_next_line(fd3);
 	printf("fd=%d : %s", fd3, line);
-	printf("(%d)\n", ret);
-	//fd---------------------------------//
-	ret = get_next_line(fd, &line);
-	printf("fd=%d : %s", fd, line);
-	printf("(%d)\n", ret);
-
+	if (!line)
+		printf("\n");
+	//fd1---------------------------------//
+	line = get_next_line(fd1);
+	printf("fd=%d : %s", fd1, line);
+	if (!line)
+		printf("\n");
 }
