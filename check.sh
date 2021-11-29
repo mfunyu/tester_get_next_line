f740f3c7d17a493af6679785aea84ca1dc8546df
mfunyu <mfunyu@student.42tokyo.jp>
2021-11-29 18:12:10 +0900

    [clean] fix loops

--- check.sh
+++ check.sh
@@ -3,33 +3,41 @@ make -s fclean
 
 #FILES-------------------------------------------------------------------------------------
 
-for FILE in box.txt empty.txt long_line.txt expected.txt fewchar_parline.txt
-# for FILE in expected.txt
-do
+FILES=`ls -1 ./txts | grep ".txt"`
+FLIELST=(${FILES// / })
+
+for FILE in "${FLIELST[@]}"; do
 
 printf "<<------------$FILE----------------->>\n"
 
 #0-19-------------------------------------------------------------------------------------
 
-for ((i=0 ; i < 19; i++))
-do
-printf '\e[36;1m[BUFFER_SIZE='$i']\e[m'
-make -sB ARG=$i
-printf " :  "
-./exec txts/$FILE > results/ac$i.txt
-# ./exec txts/$FILE | ghead -c -1 > results/ac$i.txt
-diff -c results/ac$i.txt txts/$FILE >> log.txt
-if [ $? = 1 ]; then
-	printf "\e[31;1m [KO] :(\e[m\n"
-else
-	printf "\e[32;1m [OK] :)\e[m\n"
-fi
+for ((i=0 ; i < 19; i++)); do
+	printf '\e[36;1m[BUFFER_SIZE='$i']\e[m'
+	make -sB ARG=$i
+	printf " :  "
+	./exec txts/$FILE > results/ac$i.txt
+	# ./exec txts/$FILE | ghead -c -1 > results/ac$i.txt
+	diff -c results/ac$i.txt txts/$FILE >> log.txt
+	if [ $? = 1 ]; then
+		printf "\e[31;1m [KO] :(\e[m\n"
+	else
+		printf "\e[32;1m [OK] :)\e[m\n"
+	fi
 done
 
 #any numbers-------------------------------------------------------------------------------------
 
-for i in 32 1024 1025 9999 100000 10000000
-do
+ARGS=(
+32
+1024
+1025
+9999
+100000
+10000000
+)
+
+for i in "${ARGS[@]}"; do
 printf '\e[36;1m[BUFFER_SIZE='$i']\e[m'
 make -sB ARG=$i
 printf " :  "
@@ -53,8 +61,7 @@ done
 
 printf "<<------------STD input----------------->>\n"
 
-for i in 32 1024 1025 9999 100000 10000000
-do
+for i in "${ARGS[@]}"; do
 printf '\e[36;1m[BUFFER_SIZE='$i']\e[m'
 make -s ARG=$i re
 printf " : \n"
